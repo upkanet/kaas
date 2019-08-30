c8db092e342678b882bcde3a8595272b49d66b0d
Upkanet <upkanet@gmail.com>
2019-08-30 18:53:53 +0200

    Verbose mode

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ let url = require('url');
 let robot = require('robotjs-node10');
 
 var port = 1119;
-
+var myArgs = process.argv.slice(2);
+var verbose = (myArgs[0] == 'verbose');
 
 let server = http.createServer(function(req,res){
 	
@@ -15,16 +16,18 @@ let server = http.createServer(function(req,res){
 	var resjson = {};
 
 	if(req.url == '/favicon.ico'){
+		verbose ? console.log('Catch favicon') : true;
 		headcode = 204;
 		resjson = {'status':'error','message':'favicon.ico not found'};
 	}
 	else{
 		try{
+			verbose ? console.log(cmd, arg) : true;
 			robot[cmd](arg);
 			resjson = {'status':'OK','cmd':cmd,'arg':arg};
 		}
 		catch(error){
-			console.error(query, error);
+			verbose ? console.error(query, error) : true;
 			headcode = 501;
 			resjson = {'status':'error','query':query,'error':error};
 		}
@@ -36,4 +39,5 @@ let server = http.createServer(function(req,res){
 
 
 console.log('KAAS server on port '+port+'...');
+verbose ? console.log('==Verbose mode==') : true;
 server.listen(port);
