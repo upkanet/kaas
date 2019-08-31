790f5b65a12709f4aad2b22b64d713c4544abf01
Upkanet <upkanet@gmail.com>
2019-08-31 18:33:09 +0200

    POST/JSON & multiparameter

--- server.js
+++ server.js
@@ -1,46 +1,47 @@
 //Use http to create server
 let http = require('http');
-//Use url to play with resquest url
-let url = require('url');
 //Use robot to control computer inputs (-node10 version for last NodeJS compatibility)
 let robot = require('robotjs-node10');
 
 //Specify port 1119 (for "K" "S" keyboard as a service)
 var port = 1119;
-//Check server command line arguments
-var myArgs = process.argv.slice(2);
 //Activate verbose if requested
-var verbose = (myArgs[0] == 'verbose');
+var verbose = (process.argv[2] == 'verbose');
+console.log(verbose);
 
+//Create the server handler
 let server = http.createServer(function(req,res){
-	
-	var urlObj = url.parse(req.url, true, false);
-	var query = urlObj.query;
-	var cmd = Object.keys(query)[0];
-	var arg = query[cmd];
-	var headcode = 200;
-	var resjson = {};
+	var jsres = {
+		status: 200,
+		message: null
+	};
 
-	if(req.url == '/favicon.ico'){
-		verbose ? console.log('Catch favicon') : true;
-		headcode = 204;
-		resjson = {'status':'error','message':'favicon.ico not found'};
+	if(req.method == 'POST'){
+		//Start receiving data
+		req.on('data', function(data) {
+    		var cmd = JSON.parse(data);
+    		try{
+    			robot[cmd.function].apply(null,cmd.args);
+    		}
+    		catch(e){
+    			verbose ? console.error('Function error') : null;
+    			jsres.status = 500;
+    			jsres.message = "function error";
+    		}
+    		verbose ? console.log(cmd.function, cmd.args) : null;
+    	});
+
+		//End post request
+		req.on('end',function(){
+			res.writeHead(jsres.status, {'Content-Type': 'application/json'});
+			verbose ? console.log(jsres) : null;
+			res.end(JSON.stringify(jsres));
+		});
+
 	}
 	else{
-		try{
-			verbose ? console.log(cmd, arg) : true;
-			robot[cmd](arg);
-			resjson = {'status':'OK','cmd':cmd,'arg':arg};
-		}
-		catch(error){
-			verbose ? console.error(query, error) : true;
-			headcode = 501;
-			resjson = {'status':'error','query':query,'error':error};
-		}
+		verbose ? console.log('GET') : null;
 	}
-
-	res.writeHead(headcode, {'Content-Type': 'application/json'});
-	res.end(JSON.stringify(resjson));
 });
 
 
